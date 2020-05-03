fc99fd63235f7b85808e68c28778e6b7d815711b
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-05-02 23:12:28 -0500

    Render activities as JSON

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,4 +1,6 @@
-class Api::ActivitiesController < ApplicationController
+class Api::ActivitiesController < Api::ApiController
   def index
+    # Maybe add pagination here but would increase the scope of the exercise
+    render json: ActivitySerializer.new(Activity.all, is_collection: true).serialized_json
   end
 end
