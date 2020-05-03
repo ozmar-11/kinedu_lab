8575b8b56f065941c1ee23713b7e43ee29b189d3
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-05-02 21:51:44 -0500

    Configure UTC as timezone in the application

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module KineduLab
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
   end
 end
