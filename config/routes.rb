13a909642515ecd4000102693e3c86f89f0a02c2
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-05-02 23:07:04 -0500

    Add API endpoints routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'activities/index'
+  namespace :api, defaults: { format: :json } do
+    resources :activities, only: [:index]
+    resources :activity_logs, only: [:create, :update]
+    resources :babies, only: [:index] do
+      resources :activity_logs, only: :index
+    end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
