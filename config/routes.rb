9618a516bb61b49597bfc3605564e7959062d80e
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-05-03 20:07:27 -0500

    Add sort routes and add new route
    - In order to be able to visit the admin section we add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :activity_logs, only: :index
+  end
+
   namespace :api, defaults: { format: :json } do
-    resources :activities, only: [:index]
+    resources :activities, only: :index
     resources :activity_logs, only: [:create, :update]
-    resources :babies, only: [:index] do
+    resources :babies, only: :index do
       resources :activity_logs, only: :index
     end
   end
